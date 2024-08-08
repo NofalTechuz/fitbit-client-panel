be22c9078d5f273ba49d8693736e37ea79dbe5f6
NofalTechuz <nofal@techuz.com>
2024-08-08 16:31:53 +0530

    warning msg solved

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -12,63 +12,81 @@ const Chat = () => {
   const [receiverId, setReceiverId] = useState(null);
   const [chats, setChats] = useState([]);
   const [message, setMessage] = useState('');
-  // const [file_id, setFileId] = useState(null);
+  const [selectedFiles, setSelectedFiles] = useState([]); // Updated to handle multiple files
   const [senderName, setSenderName] = useState({});
   const [receiverName, setReceiverName] = useState({});
+  const [fetchChatStatus, setFetchChatStatus] = useState(false);
 
-  const fetchUsers = async () => {
-    setLoading(true);
-    try {
-      const response = await axiosInstance.get('/user');
-      setUsers(response.data);
-
-      if (response.data.length > 0) {
-        setReceiverId(response.data[0].id);
-      }
-
-      setSenderName(response.data.find((user) => user.id === parseInt(id)));
-    } catch (error) {
-      console.error(error);
-    }
-    setLoading(false);
-  };
-
-  const fetchChats = async () => {
-    try {
-      const response = await axiosInstance.get(`/chats/${senderId}/${receiverId}`);
-      setChats(response.data);
-
-      setReceiverName(users.find((user) => user.id === parseInt(receiverId)));
-    } catch (error) {
-      console.error(error);
+  const handleMediaUpload = (e) => {
+    const files = Array.from(e.target.files);
+    if (files.length + selectedFiles.length <= 5) {
+      setSelectedFiles([...selectedFiles, ...files]);
+    } else {
+      alert('You can upload a maximum of 5 files.');
     }
   };
 
-  // Send Message
+  const removeSelectedFile = (index) => {
+    const updatedFiles = selectedFiles.filter((_, i) => i !== index);
+    setSelectedFiles(updatedFiles);
+  };
+
   const sendMessage = async (e) => {
     e.preventDefault();
     try {
+      // Assuming you want to upload files and send their IDs
+      const fileNames = selectedFiles.map((file) => file.name);
+
       await axiosInstance.post('/chats', {
         sender_id: senderId,
         reciver_id: receiverId,
         message,
-        chat_file_id: null,
+        chat_file_id: fileNames.length > 0 ? fileNames.join(',') : null,
       });
-      fetchChats();
       setMessage('');
+      setSelectedFiles([]); // Clear the selected files after sending
+      setFetchChatStatus(!fetchChatStatus);
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    fetchChats();
-  }, [receiverId]);
+    const fetchChats = async () => {
+      try {
+        const response = await axiosInstance.get(`/chats/${senderId}/${receiverId}`);
+        setChats(response.data);
 
+        setReceiverName(users.find((user) => user.id === parseInt(receiverId)));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchChats();
+  }, [receiverId, senderId, users, fetchChatStatus]);
+
+  
   useEffect(() => {
+    const fetchUsers = async () => {
+      setLoading(true);
+      try {
+        const response = await axiosInstance.get('/user');
+        setUsers(response.data);
+
+        if (response.data.length > 0) {
+          setReceiverId(response.data[0].id);
+        }
+
+        setSenderName(response.data.find((user) => user.id === parseInt(id)));
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
+    };
     fetchUsers();
-    setSenderId(id)
-  }, []);
+    setSenderId(id);
+  }, [id]);
 
   if (loading) {
     return (
@@ -84,7 +102,6 @@ const Chat = () => {
         <div className="orders">
           <div className="chat">
             <div className="chat__sidebar">
-              {/* <h2>Open Chat</h2> */}
               <div>
                 <h4 className="chat__header" style={{ marginTop: 0 }}>
                   USERS
@@ -119,7 +136,9 @@ const Chat = () => {
                 <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                   <div className="chat__userImg__Profile">
                     <img
-                      src={'/assets/img/defultavtar.png'}
+                      src={
+                        receiverName?.profileImage !== null ? receiverName?.profileImage : '/assets/img/defultavtar.png'
+                      }
                       alt="Profile"
                       height={50}
                       width={50}
@@ -130,7 +149,7 @@ const Chat = () => {
                     <p className="user__name">{receiverName?.firstName + ' ' + receiverName?.lastName}</p>
                   </div>
                 </div>
-                <button className="leaveChat__btn">LEAVE CHAT</button>
+                {/* <button className="leaveChat__btn">LEAVE CHAT</button> */}
               </header>
               <div className="message__container">
                 {chats.length > 0
@@ -141,17 +160,34 @@ const Chat = () => {
                             ? senderName?.firstName + ' ' + senderName?.lastName
                             : receiverName?.firstName + ' ' + receiverName?.lastName}
                         </p>
-                        <div className={chat.sender_id === parseInt(senderId) ? 'message__sender' : 'message__recipient'}>
+                        <div
+                          className={chat.sender_id === parseInt(senderId) ? 'message__sender' : 'message__recipient'}
+                        >
                           <p>{chat.message}</p>
                         </div>
                       </div>
                     ))
                   : null}
-                {/* <div className="message__status">
-                  <p>Typing status...</p>
-                </div> */}
               </div>
               <div className="chat__footer">
+                {selectedFiles.length > 0 && (
+                  <div className="selectedFile-container">
+                    {selectedFiles.map((file, index) => (
+                      <div className="selectedFile" key={index}>
+                        <div style={{ display: 'flex', alignItems: 'start', justifyContent: 'end' }}>
+                          <button type="button" className="removeFileBtn" onClick={() => removeSelectedFile(index)}>
+                            <i className="bx bx-x"></i>
+                          </button>
+                        </div>
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                          <i className="bx bxs-file-blank" style={{ fontSize: '50px' }}></i>
+                        </div>
+                        <p style={{ marginTop: '10px' }}>{file.name.substring(0, 10)}...</p>
+                      </div>
+                    ))}
+                  </div>
+                )}
+
                 <form className="form">
                   <input
                     type="text"
@@ -160,6 +196,20 @@ const Chat = () => {
                     onChange={(e) => setMessage(e.target.value)}
                     value={message}
                   />
+
+                  <label htmlFor="mediaUpload" className="uploadBtn">
+                    <i className="bx bx-paperclip"></i>
+                  </label>
+
+                  <input
+                    type="file"
+                    id="mediaUpload"
+                    className="mediaUpload"
+                    accept="image/*,video/*"
+                    onChange={handleMediaUpload}
+                    multiple
+                  />
+
                   <button className="sendBtn" onClick={sendMessage}>
                     SEND
                   </button>
